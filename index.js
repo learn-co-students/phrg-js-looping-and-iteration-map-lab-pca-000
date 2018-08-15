347093ce02bb89794f06266b0e60f453e10b2175
Katie Weber <k.weber0893@gmail.com>
2018-08-15 11:58:10 -0400

    Map

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution in this file.
+function lowerCaseDrivers(drivers) {
+  return drivers.map(function (name) {
+    return name.toLowerCase()
+  })
+}
+
+function nameToAttributes(drivers) {
+  return drivers.map(function (name) {
+    const firstName = name.split(" ")[0];
+    const lastName = name.split(" ")[1];
+
+    return { firstName: firstName, lastName: lastName };
+  })
+}
+
+function attributesToPhrase(drivers) {
+  return drivers.map(function (object) {
+    return `${object.name} is from ${object.hometown}`
+  })
+ }
