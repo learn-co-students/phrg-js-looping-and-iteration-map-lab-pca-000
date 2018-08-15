74d41b673878c42e2b122e47cfa41ff87876d6be
Daniel Mack <daniel.mack@powerhrg.com>
2018-08-15 11:23:21 -0400

    finished lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file.
+
+function lowerCaseDrivers(drivers) {
+  const emptyArray = []
+  for (const driver of drivers) {
+    emptyArray.push(driver.toLowerCase())
+  }
+  return emptyArray
+}
+
+function nameToAttributes(drivers) {
+  const driverNames = drivers.map(function (name) {
+    let firstName1 = name.split(" ")[0]
+    let lastName1 = name.split(" ")[1]
+    return {firstName: firstName1, lastName: lastName1}
+  })
+  return driverNames
+}
+
+function attributesToPhrase(drivers) {
+  const driverInfo = drivers.map(function (driver){
+    name = driver["name"]
+    hometown = driver["hometown"]
+    return `${name} is from ${hometown}`
+  })
+  return driverInfo
+}
