d2c547cc63d97a2624a43225f285b73d0fcbadf9
Ezz Abousaif <ezzeldin.abousaif@powerhrg.com>
2019-06-18 10:53:28 -0400

    mapping lab - JS functions are getting a bit easier to write now

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file.
+function lowerCaseDrivers(drivers, name) {
+  return drivers.map(function(driverName){
+    return driverName.toLowerCase()
+  })
+}
+
+function nameToAttributes(drivers, name) {
+  return drivers.map(function (driverName) {
+    const firstName = driverName.split(" ")[0];
+    const lastName = driverName.split(" ")[1];
+    return { firstName: firstName, lastName: lastName };
+  })
+}
+
+function attributesToPhrase(drivers, name, hometown) {
+  return drivers.map(function(phrase){
+    return `${phrase.name} is from ${phrase.hometown}`
+  })
+}
