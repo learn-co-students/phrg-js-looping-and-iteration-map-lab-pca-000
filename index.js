bc1b521e9675f5f1abf89f4775f424ec9135210d
KFad <kfadigan@yahoo.com>
2019-06-19 10:46:08 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution in this file.
+function map (array, callback) {
+  const newArr = [];
+
+  for (const element of array) {
+    newArr.push(callback(element));
+  }
+
+  return newArr;
+}
+
+function lowerCaseDrivers(drivers) {
+  const lowCase = map(drivers, function (name) { return name.toLowerCase()})
+
+  return lowCase
+}
+
+function nameToAttributes(drivers) {
+  const attrs = map(drivers, function (name) {
+    const firstName = name.split(" ")[0];
+    const lastName = name.split(" ")[1];
+    return { firstName: firstName, lastName: lastName };
+  })
+
+  return attrs
+}
+
+function attributesToPhrase(drivers, name, hometown) {
+  const attributes = map(drivers, function(phrase){
+    return `${phrase.name} is from ${phrase.hometown}`
+  })
+
+  return attributes
+}
